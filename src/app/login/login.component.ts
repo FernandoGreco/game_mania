b653879d3432d90467fc6c9d152198d2a7191a1f
greco874 <fgreco@mtsolucoes.com.br>
2022-06-14 22:09:37 -0300

    login com validação

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
-
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,14 @@ export class LoginComponent implements OnInit {
 
   hide: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router) {
   }
 
   ngOnInit() {
   }
+
+  loginModel = new User();
+  mensagem = ""
 
   loginForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -28,6 +33,21 @@ export class LoginComponent implements OnInit {
       return;
     }
     console.log(this.loginForm.value);
+
+    
+    this.loginService.login(this.loginModel).subscribe((response)=>{
+      this.router.navigateByUrl('')
+    }, (respostaErro)=>{
+      this.mensagem = respostaErro.console.error()
+      console.log(this.mensagem)
+      
+    
+    })
+   
   }
 
+
+
+
+ 
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient : HttpClient) { }
+
+  url = "http://localhost:3000/users"
+
+  login(user: User): Observable<any>{
+    return this.httpClient.post(this.url, JSON.stringify(user),{
+      headers: new HttpHeaders({'Content-Type' :  'application/json'}),
+      observe: 'response'
+    })
+  }
+
+
+
+
+}
